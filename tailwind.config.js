b288d15b1937c0069d63b15ffbc920c8a90d5d48
HTML <junkerhendrik@googlemail.com>
2026-06-18 07:14:55 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./api/**/*.js",
+    "./sw.js",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
